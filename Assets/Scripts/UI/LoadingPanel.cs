b2da7604cd33174f457ec4706a203f7a6853675c
TripleMonster <wang1013tao@gmail.com>
2018-06-04 15:38:32 +0800

    重新Loading界面, 重新梳理文件目录结构

--- Assets/Scripts/UI/LoadingPanel.cs
+++ Assets/Scripts/UI/LoadingPanel.cs
@@ -2,25 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LoadingPanel : MonoBehaviour {
-	public Slider _loadingSlider;
-	private int loadingStep;
-	private bool beginLoading;
+    [SerializeField] private Slider _loadingSlider;
+
+    enum LoadingStep { LOADING_START, LOADING_CONFIG, LOADING_ATLAS, LOADING_PREFAB, LOADING_AUDIO, LOADING_END }
+    LoadingStep curStep;
+    bool isStartLoading = true;
 
 	void Start () 
 	{
-		
+        curStep = LoadingStep.LOADING_START;
 	}
 
 	void Update () 
 	{
-		if (beginLoading)
-			BeginLoading();
+        if (isStartLoading)
+        {
+            isStartLoading = false;
+            StartCoroutine(Loading());
+        }
 	}
 
-	void BeginLoading()
-	{
-		
-	}
+    IEnumerator Loading()
+    {
+        LoadingByStep(curStep);
+        yield return new WaitForSeconds(2.0f);
+        isStartLoading = true;
+    }
+
+    void LoadingByStep(LoadingStep step)
+    {
+        switch (step)
+        {
+            case LoadingStep.LOADING_START:
+                {
+                    curStep = LoadingStep.LOADING_CONFIG;
+                }
+                break;
+            case LoadingStep.LOADING_CONFIG:
+                {
+                    curStep = LoadingStep.LOADING_ATLAS;
+                    //LoadingConfigs();
+                }
+                break;
+            case LoadingStep.LOADING_ATLAS:
+                {
+                    curStep = LoadingStep.LOADING_PREFAB;
+                    //LoadingAtlases();
+                }
+                break;
+            case LoadingStep.LOADING_PREFAB:
+                {
+                    curStep = LoadingStep.LOADING_AUDIO;
+                    _loadingSlider.value += 0.2f;
+                }
+                break;
+            case LoadingStep.LOADING_AUDIO:
+                {
+                    curStep = LoadingStep.LOADING_END;
+                    _loadingSlider.value += 0.2f;
+                }
+                break;
+            case LoadingStep.LOADING_END:
+                {
+                    isStartLoading = false;
+                    AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("SampleScene");
+                }
+                break;
+        }      
+    }
 }
