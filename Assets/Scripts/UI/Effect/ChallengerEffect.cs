0f8a48cd5279fcb8711a04cbacfedebb6ed2f07c
TripleMonster <wang1013tao@gmail.com>
2018-06-29 16:48:59 +0800

    增加新的UI图片(部分icon和arena); 增加主界面动画

---
+++ Assets/Scripts/UI/Effect/ChallengerEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ChallengerEffect : MonoBehaviour 
+{
+	float moveOffset = 10;
+	float positionY;
+	bool beginMove;
+	void Start () 
+	{
+		positionY = transform.position.y;
+		beginMove = true;
+	}
+	
+	void Update () 
+	{
+		if (beginMove)
+		{
+			beginMove = false;
+			transform.DOMoveY(positionY + moveOffset, 2.5f).OnComplete(RevertDirection);
+		}
+	}
+
+	void RevertDirection()
+	{
+		moveOffset = -moveOffset;
+		beginMove = true;
+	}
+}
