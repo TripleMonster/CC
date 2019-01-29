75eb4e5231e9e2385b0c423390f0c24112fde7be
TripleMonster <wang1013tao@gmail.com>
2019-01-29 17:28:26 +0800

    地图选中模式 测试

---
+++ Assets/Scripts/Test/MapTest/MapController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public List<RegionController> _regionsList;
+
+    int mCurSelectedID = 0;
+
+    void Start()
+    {
+        if (_regionsList != null) 
+        {
+            foreach (var item in _regionsList)
+            {
+                item._SelectedEvent.AddListener(OnSelectedRegionByID);
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnSelectedRegionByID(int id) 
+    {
+        int reID = id - 1;
+        if (reID > _regionsList.Count || reID < 0)
+            return;
+
+        Debug.Log("当前选中的区域为 : " + id + ", 上次选中区域为 :" + mCurSelectedID);
+
+        if (mCurSelectedID != id && mCurSelectedID > 0)
+        {
+            int pID = mCurSelectedID - 1;
+            pID = pID < 0 ? 0 : pID;
+            RegionController regionCtr = _regionsList[pID];
+            regionCtr.LostSelectedState();
+        }
+
+        mCurSelectedID = id;
+    }
+}
---
+++ Assets/Scripts/Test/MapTest/RegionController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEventUtils;
+using SpriteGlow;
+
+public class RegionController : MonoBehaviour
+{
+    public int _ID;
+    public UEvent_i _SelectedEvent = new UEvent_i();
+    public SpriteGlowEffect _SelectedEffect;
+    [HideInInspector] public bool isSelected;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown() 
+    {
+        if (isSelected)
+            return;
+
+
+        ShowSetectedEffect();
+
+        if (_SelectedEvent != null)
+            _SelectedEvent.Invoke(_ID);
+    }
+
+    void ShowSetectedEffect()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, -1);
+
+        _SelectedEffect.GlowBrightness = 4;
+        _SelectedEffect.OutlineWidth = 1;
+        _SelectedEffect.AlphaThreshold = 0.1f;
+    }
+
+    void HideSelectedEffect()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+
+        _SelectedEffect.GlowBrightness = 1;
+        _SelectedEffect.OutlineWidth = 0;
+        _SelectedEffect.AlphaThreshold = 0;
+    }
+
+    public void LostSelectedState()
+    {
+        Debug.Log("失去选中效果的区域为 : " + _ID);
+        HideSelectedEffect();
+        isSelected = false;
+    }
+}
