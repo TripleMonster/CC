b3ec864caf6ddd2c57273f43d04479df4cd563d2
TripleMonster <wang1013tao@gmail.com>
2019-01-30 18:40:17 +0800

    世界坐标下的拖拽

--- Assets/Scripts/Test/MapTest/DragButton.cs
+++ Assets/Scripts/Test/MapTest/DragButton.cs
@@ -8,10 +8,13 @@ using UnityEngine.EventSystems;
 public class DragButton : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Canvas _Canvas;
+    public GameObject _ArmysObj;
+    public Sprite _ArmySprite;
 
     private RectTransform mDragImageRT;
     private Vector3 mDragImagePosition;
     private GameObject mDragingObj;
+    private Sprite mDragingSprite;
 
     private void Awake() 
     {
@@ -43,6 +46,20 @@ public class DragButton : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
         SetDraggedPosition(eventData);
     }
 
+    void GenerateDragingWorldObj(PointerEventData eventData)
+    {
+        mDragingObj = new GameObject("Army");
+        mDragingObj.transform.SetParent(_ArmysObj.transform, false);
+        mDragingObj.transform.SetAsLastSibling();
+        mDragingObj.transform.localScale = new Vector3(3, 3, 0);
+
+        var spRenderer = mDragingObj.AddComponent<SpriteRenderer>();
+        spRenderer.sprite = _ArmySprite;
+        spRenderer.color = new Color32(35, 183, 70, 255);
+
+        SetDraggedPosition2(eventData.position);
+    }
+
     void SetDraggedPosition(PointerEventData eventData)
     {
         var rt = mDragingObj.GetComponent<RectTransform>();
@@ -53,17 +70,28 @@ public class DragButton : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
         }
     }
 
+    void SetDraggedPosition2(Vector3 pos)
+    {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(mDragingObj.transform.position);
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(pos.x, pos.y, screenPos.z));
+        mDragingObj.transform.position = new Vector3(worldPos.x, worldPos.y, -2);
+    }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         mDragImagePosition = mDragImageRT.transform.position;
 
-        GenerateDragingObj(eventData);
+        //GenerateDragingObj(eventData);
+        GenerateDragingWorldObj(eventData);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        // if (mDragingObj != null)
+        //     SetDraggedPosition(eventData);
+
         if (mDragingObj != null)
-            SetDraggedPosition(eventData);
+            SetDraggedPosition2(eventData.position);
     }
 
     public void OnEndDrag(PointerEventData eventData)
